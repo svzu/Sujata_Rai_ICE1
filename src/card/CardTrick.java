c6237b8d65bd353ad5bed2a25d2e58571368886d
svzu <133915562+svzu@users.noreply.github.com>
2023-05-26 17:45:26 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -8,7 +8,7 @@ package card;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * @author Sujata Rai
  */
 import java.util.*;
 
